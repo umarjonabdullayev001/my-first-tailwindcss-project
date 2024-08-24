0d7225864c4f2f30534f59eae1064b98c5d7ae3b
umarjonabdullayev001 <umarjonabdullayev001@gmail.com>
2024-08-24 17:43:36 +0500

    changet-team-section

--- app.js
+++ app.js
@@ -8,18 +8,3 @@ fetch("footer.html")
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
   });
-
-// Hozirgi sahifa nomini olish
-const currentPage = window.location.pathname.split("/").pop().split(".")[0];
-
-// Hozirgi sahifadagi navbar elementini faollashtirish
-const activeLink = document.getElementById(currentPage);
-if (activeLink) {
-  activeLink.classList.add("text-blue-500", "font-bold"); // Oynaning faollashishini ko'rsatish uchun sinflar qo'shiladi
-}
-
-var map = L.map("map").setView([51.505, -0.09], 13);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-}).addTo(map);
